21055614af6a15ab4826c833502fc6705a5ae124
ryanmaster33 <85665855+ryanmaster33@users.noreply.github.com>
2021-08-30 12:44:15 +0700

    Added search and filtering in table

--- src/Components/SidebarData.js
+++ src/Components/SidebarData.js
@@ -11,7 +11,7 @@ export const SidebarData = [
         cName: 'nav-text'
     },
     {
-     title: 'Reports',
+        title: 'Reports',
         path: '/reports',
         icon: <IoIcons.IoIosPaper/>,
         cName: 'nav-text'
